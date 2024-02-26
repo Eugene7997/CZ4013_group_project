f781218be5d64c3ba422c0319dcab1e1e4f3ee5e
Yi Pun Wong <wongyipun99@gmail.com>
2024-02-27 00:06:09 +0800

    lint: fix `flake8` violations

--- tests/test_message.py
+++ tests/test_message.py
@@ -5,10 +5,7 @@ class TestMessage:
     @staticmethod
     def test_unmarshall():
         read_file_request: ReadFileRequest = ReadFileRequest(
-            request_id=155,
-            offset=777,
-            read_bytes=15,
-            filename="random_file_name"
+            request_id=155, offset=777, read_bytes=15, filename="random_file_name"
         )
         marshalled_data: bytes = read_file_request.marshall()
         unmarshalled_obj: Message = Message.unmarshall(marshalled_data)
@@ -19,10 +16,7 @@ class TestReadFileRequest:
     @staticmethod
     def test_marshall_unmarshall():
         read_file_request: ReadFileRequest = ReadFileRequest(
-            request_id=123,
-            offset=456,
-            read_bytes=15,
-            filename="random_file_name"
+            request_id=123, offset=456, read_bytes=15, filename="random_file_name"
         )
         marshalled_data: bytes = read_file_request.marshall()
         unmarshalled_obj: Message = Message.unmarshall(marshalled_data)
@@ -33,10 +27,7 @@ class TestWriteFileRequest:
     @staticmethod
     def test_marshall_unmarshall():
         write_file_request: WriteFileRequest = WriteFileRequest(
-            request_id=123,
-            offset=456,
-            file_name="random_file",
-            content=b"random_file_content"
+            request_id=123, offset=456, file_name="random_file", content=b"random_file_content"
         )
         marshalled_data: bytes = write_file_request.marshall()
         unmarshalled_obj: Message = Message.unmarshall(marshalled_data)
@@ -47,11 +38,11 @@ class TestSubscribeToUpdatesRequest:
     @staticmethod
     def test_marshall_unmarshall():
         subscribe_request: SubscribeToUpdatesRequest = SubscribeToUpdatesRequest(
-            client_ip_address='192.168.255.255',
+            client_ip_address="192.168.255.255",
             client_port_number=123,
             monitoring_interval=15,
             file_name="random_file_name",
-            file_name_length=len("random_file_name")
+            file_name_length=len("random_file_name"),
         )
         marshalled_data: bytes = subscribe_request.marshall()
         unmarshalled_obj: Message = Message.unmarshall(marshalled_data)
