c36799d61b62bc4e8a50cf9a7040c3a3e19d4501
Yi Pun Wong <wongyipun99@gmail.com>
2024-03-20 21:07:14 +0800

    fix: use relative paths when interacting with file system

--- remote_file_system/server_interface.py
+++ remote_file_system/server_interface.py
@@ -4,47 +4,57 @@ from collections import defaultdict
 from ipaddress import IPv4Address
 from typing import Dict, Tuple, List
 
+from loguru import logger
+
 
 class Server:
-    def __init__(self, file_storage_location: str):
+    def __init__(self, server_root_directory: str):
+        # TODO change subscribed_client into a class or named tuple
         self.subscribed_clients: Dict[str, List[Tuple[int, IPv4Address, int]]] = defaultdict(list)
-        self.root_directory: str = file_storage_location
-        pass
+        self.server_root_directory: str = server_root_directory
 
-    def read_file(self, file_name: str) -> bytes:
-        file_path = os.path.join(self.root_directory, file_name)
+    def read_file(self, relative_file_path: str) -> bytes:
+        full_file_path = os.path.join(self.server_root_directory, relative_file_path)
 
-        if not os.path.exists(file_path):
+        if not os.path.exists(full_file_path):
+            logger.warning(f"Server failed to perform a read file operation as no file exists at {full_file_path}")
             return None
 
-        with open(file_path, "rb") as file:
+        with open(full_file_path, "rb") as file:
             file_contents = file.read()
             return file_contents
 
-    def write_file(self, file_name: str, offset: int, file_content: bytes) -> List[Tuple[IPv4Address, int]]:
-        file_path = os.path.join(self.root_directory, file_name)
+    def write_file(self, relative_file_path: str, offset: int, file_content: bytes) -> List[Tuple[IPv4Address, int]]:
+        full_file_path = os.path.join(self.server_root_directory, relative_file_path)
 
-        if not os.path.exists(file_path):
+        if not os.path.exists(full_file_path):
+            logger.warning(f"Server failed to perform a write file operation as no file exists at {full_file_path}")
             return False, None
 
-        with open(file_path, "r+b") as file:
+        with open(full_file_path, "r+b") as file:
             file.seek(offset)
             file.write(file_content)
 
-        return True, self.subscribed_clients[file_name]
+        return True, self.subscribed_clients[relative_file_path]
 
-    def get_modified_timestamp(self, file_path: str) -> Tuple[bool, int]:
-        if not os.path.exists(file_path):
+    def get_modified_timestamp(self, relative_file_path: str) -> Tuple[bool, int]:
+        full_file_path = os.path.join(self.server_root_directory, relative_file_path)
+
+        if not os.path.exists(full_file_path):
+            logger.warning(
+                f"Server failed to check a file modification timestamp as no file exists at {full_file_path}"
+            )
             return False, None
-        timestamp_of_modifications_to_file_in_seconds: int = int(os.path.getmtime(file_path))
-        return True, timestamp_of_modifications_to_file_in_seconds
+
+        modification_timestamp: int = int(os.path.getmtime(full_file_path))
+        return True, modification_timestamp
 
     def subscribe_to_updates(
         self,
         client_ip_address: IPv4Address,
         client_port_number: int,
         monitoring_interval_in_seconds: int,
-        file_name: str,
+        relative_file_path: str,
     ) -> bool:
         current_timestamp: int = int(time.time())
         monitoring_expiration_timestamp: int = current_timestamp + monitoring_interval_in_seconds
@@ -53,5 +63,5 @@ class Server:
             client_ip_address,
             client_port_number,
         )
-        self.subscribed_clients[file_name].append(subscribed_client)
+        self.subscribed_clients[relative_file_path].append(subscribed_client)
         return True
