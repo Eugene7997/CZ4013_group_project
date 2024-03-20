3bcd78fce7af6e9dfbe4b00be7bc4faea025a219
Yi Pun Wong <wongyipun99@gmail.com>
2024-03-20 21:06:11 +0800

    test: use new mock data directory

--- tests/test_server_interface.py
+++ tests/test_server_interface.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 
 from remote_file_system.server_interface import Server
 
@@ -8,7 +8,7 @@ class TestServerInterface:
 
     @staticmethod
     def test_read_file_file_exists():
-        server = Server(file_storage_location=os.getcwd())
-        actual = server.read_file("tests/mock_data/english_alphabets.txt")
+        server = Server(server_root_directory=Path.cwd() / "tests" / "server")
+        actual = server.read_file("english_alphabets.txt")
         expected = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".encode("UTF-8")
         assert actual == expected
