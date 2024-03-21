81287ff6b30345866923870d25f3efb7bbaa4b27
john <ypoh007@e.ntu.edu.sg>
2024-03-21 12:46:00 +0800

    added testing for delete file

--- tests/test_server_interface.py
+++ tests/test_server_interface.py
@@ -12,3 +12,11 @@ class TestServerInterface:
         actual = server.read_file("tests/mock_data/english_alphabets.txt")
         expected = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".encode("UTF-8")
         assert actual == expected
+    
+    def test_delete_file():
+        server_file_directory = os.getcwd()
+        f = open(f"{server_file_directory}/deleteme.txt", "w")
+        server = Server(file_storage_location=server_file_directory)
+        actual = server.delete_file("deleteme.txt")
+        expected = True
+        assert actual == expected
